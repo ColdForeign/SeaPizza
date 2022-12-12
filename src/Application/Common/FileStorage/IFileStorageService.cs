92be3c3769a89630c5993b06cae6f5d46aa4e131
ColdForeign <tankon46555@gmail.com>
2022-12-12 11:23:45 +0200

    Added newline at end of files.

--- src/Application/Common/FileStorage/IFileStorageService.cs
+++ src/Application/Common/FileStorage/IFileStorageService.cs
@@ -8,7 +8,7 @@ namespace SeaPizza.Application.Common.FileStorage;
 public interface IFileStorageService : ITransientService
 {
     public Task<string> UploadAsync<T>(FileUploadRequest? request, FileType supportedFileType, CancellationToken cancellationToken = default)
-    where T : class;
+        where T : class;
 
     public void Remove(string? path);
 }
