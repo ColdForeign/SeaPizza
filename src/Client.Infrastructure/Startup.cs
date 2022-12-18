15023c4dea5a861827a99411cd03e00bf8b5bc6c
ColdForeign <tankon46555@gmail.com>
2022-12-18 11:21:45 +0200

    Configure Server. Create API generation script.

---
+++ src/Client.Infrastructure/Startup.cs
@@ -0,0 +1,55 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor;
+using MudBlazor.Services;
+using SeaPizza.Client.Infrastructure.ApiClient;
+using SeaPizza.Client.Infrastructure.Common;
+using SeaPizza.Client.Infrastructure.UserPreferences;
+using System.Linq;
+
+namespace SeaPizza.Client.Infrastructure;
+
+public static class Startup
+{
+    public static IServiceCollection AddClientServices(this IServiceCollection services, IConfiguration config) =>
+        services
+            .AddBlazoredLocalStorage()
+            .AddMudServices(configuration =>
+            {
+                configuration.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+                configuration.SnackbarConfiguration.HideTransitionDuration = 100;
+                configuration.SnackbarConfiguration.ShowTransitionDuration = 100;
+                configuration.SnackbarConfiguration.VisibleStateDuration = 3000;
+                configuration.SnackbarConfiguration.ShowCloseIcon = false;
+            })
+            .AddScoped<IClientPreferenceManager, ClientPreferenceManager>()
+            .AutoRegisterInterfaces<IAppService>()
+            .AutoRegisterInterfaces<IApiService>();
+
+    public static IServiceCollection AutoRegisterInterfaces<T>(this IServiceCollection services)
+    {
+        var @interface = typeof(T);
+
+        var types = @interface
+            .Assembly
+            .GetExportedTypes()
+            .Where(t => t.IsClass && !t.IsAbstract)
+            .Select(t => new
+            {
+                Service = t.GetInterface($"I{t.Name}"),
+                Implementation = t
+            })
+            .Where(t => t.Service != null);
+
+        foreach (var type in types)
+        {
+            if (@interface.IsAssignableFrom(type.Service))
+            {
+                services.AddTransient(type.Service, type.Implementation);
+            }
+        }
+
+        return services;
+    }
+}
--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,10 +1,13 @@
 using SeaPizza.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SeaPizza.Client.Infrastructure;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddClientServices(builder.Configuration);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
